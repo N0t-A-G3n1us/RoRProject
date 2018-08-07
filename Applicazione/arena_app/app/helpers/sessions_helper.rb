0cb8d66e661d555e670ed0fc671e662eaacffd7b
salva-95 <salva-95@hotmail.it>
2018-08-07 11:38:48 +0200

    implementazione remember me login

--- Applicazione/arena_app/app/helpers/sessions_helper.rb
+++ Applicazione/arena_app/app/helpers/sessions_helper.rb
@@ -5,9 +5,30 @@ module SessionsHelper
     session[:gamer_id] = gamer.id
   end
 
-  # Returns the current logged-in user (if any).
+  # Returns the gamer corresponding to the remember token cookie.
   def current_gamer
-    @current_gamer ||= Gamer.find_by(id: session[:gamer_id])
+    if (gamer_id = session[:gamer_id])
+      @current_gamer ||= Gamer.find_by(id: gamer_id)
+    elsif (gamer_id = cookies.signed[:gamer_id])
+      gamer = Gamer.find_by(id: gamer_id)
+      if gamer && gamer.authenticated?(cookies[:remember_token])
+        log_in gamer
+        @current_gamer = gamer
+      end
+    end
+  end
+  # Remembers a gamer in a persistent session.
+  def remember(gamer)
+    gamer.remember
+    cookies.permanent.signed[:gamer_id] = gamer.id
+    cookies.permanent[:remember_token] = gamer.remember_token
+  end
+
+  # Forgets a persistent session.
+  def forget(gamer)
+    gamer.forget
+    cookies.delete(:gamer_id)
+    cookies.delete(:remember_token)
   end
 
   # Returns true if the gamer is logged in, false otherwise.
@@ -15,8 +36,9 @@ module SessionsHelper
     !current_gamer.nil?
   end
 
-  # Logs out the current user.
+  # Logs out the current gamer.
   def log_out
+    forget(current_gamer)
     session.delete(:gamer_id)
     @current_gamer = nil
   end
