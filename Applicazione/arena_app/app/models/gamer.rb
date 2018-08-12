fe183ed44109d29262ae40ec587d4fd829881af3
Giuppo <romagiuppo@gmail.com>
2018-08-12 18:51:38 +0200

    deploy signup view changes

--- Applicazione/arena_app/app/models/gamer.rb
+++ Applicazione/arena_app/app/models/gamer.rb
@@ -44,6 +44,8 @@ class Gamer < ApplicationRecord
 
 
     # Returns true if the given token matches the digest.
+
+
     def authenticated?(attribute ,token)
         digest = send("#{attribute}_digest")
         return false if digest.nil?
