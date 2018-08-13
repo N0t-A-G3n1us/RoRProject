69e5974f950c40143c569bad260e702338e4fed2
Giuppo <romagiuppo@gmail.com>
2018-08-13 18:18:22 +0200

    asd

--- Applicazione/arena_app/db/schema.rb
+++ Applicazione/arena_app/db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_085402) do
     t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
-    t.time "reset_sent_at"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_gamers_on_email", unique: true
   end
 
