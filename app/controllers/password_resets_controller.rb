cf5a746a697faec6e1c3977dff263cc5de15f1f7
salva-95 <salva-95@hotmail.it>
2018-08-18 17:57:57 +0200

    update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,7 +12,7 @@ class PasswordResetsController < ApplicationController
       @gamer.create_reset_digest
       @gamer.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
-      redirect_to @gamer
+      redirect_to root_url
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
