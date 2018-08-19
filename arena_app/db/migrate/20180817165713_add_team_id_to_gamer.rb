28cead06a87955b5c47263e7c4ab3eea83c62422
Giuppo <romagiuppo@gmail.com>
2018-08-19 02:29:30 +0200

    2° attempt to solve deploy problem

--- arena_app/db/migrate/20180817165713_add_team_id_to_gamer.rb
+++ arena_app/db/migrate/20180817165713_add_team_id_to_gamer.rb
@@ -1,5 +1,4 @@
 class AddTeamIdToGamer < ActiveRecord::Migration[5.2]
   def change
-    add_reference :gamers, :team_id, foreign_key: true
   end
 end
--- arena_app/db/migrate/20180817203147_change_team_id_in_team_to_teams.rb
+++ arena_app/db/migrate/20180817203147_change_team_id_in_team_to_teams.rb
@@ -1,5 +1,4 @@
 class ChangeTeamIdInTeamToTeams < ActiveRecord::Migration[5.2]
   def change
-  	    rename_column :gamers, :team_id_id, :team_id
   end
 end
