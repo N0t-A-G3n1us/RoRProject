155b2ea2271ce8b00a994f8caece46b0386e8245
Giuppo <romagiuppo@gmail.com>
2018-08-20 10:53:25 +0200

    up

--- arena_app/app/controllers/static_pages_controller.rb
+++ arena_app/app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
 
 
   def home
+  	if logged_in? 
+  		#redirect_to controller: 'arena_pages',action: arena
+  		render "arena_pages/arena"
+  	end
   end
 
   def help
