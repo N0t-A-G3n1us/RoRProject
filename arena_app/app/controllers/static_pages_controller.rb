faecda248da551b3f1eafd41daa23af9dc94b355
salva-95 <salva-95@hotmail.it>
2018-08-22 16:06:48 +0200

    ...

--- arena_app/app/controllers/static_pages_controller.rb
+++ arena_app/app/controllers/static_pages_controller.rb
@@ -2,10 +2,6 @@ class StaticPagesController < ApplicationController
 
 
   def home
-  	if logged_in? 
-  		#redirect_to controller: 'arena_pages',action: arena
-  		render "arena_pages/arena"
-  	end
   end
 
   def help
