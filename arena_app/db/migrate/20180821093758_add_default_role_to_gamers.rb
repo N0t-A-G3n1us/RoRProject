78fe99a8a118205e3d06407670470f843571c040
Giuppo <romagiuppo@gmail.com>
2018-08-25 19:11:16 +0200

    deploy

--- arena_app/db/migrate/20180821093758_add_default_role_to_gamers.rb
+++ arena_app/db/migrate/20180821093758_add_default_role_to_gamers.rb
@@ -1,5 +1,5 @@
 class AddDefaultRoleToGamers < ActiveRecord::Migration[5.2]
   def change
-	change_column :gamers_groups, :role,:null => false,:default => 0
+	change_column :gamers_groups, :role,:integer,:null => false,:default => 0
   end
 end
