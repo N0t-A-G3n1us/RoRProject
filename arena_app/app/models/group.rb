240c960944f865b967e4b20ee1eefaf11b424176
Giuppo <romagiuppo@gmail.com>
2018-08-31 12:39:04 +0200

    small fix to group controller

--- arena_app/app/models/group.rb
+++ arena_app/app/models/group.rb
@@ -6,12 +6,12 @@ class Group < ApplicationRecord
   #validates :games,length:{minimum:1} #not working
   
 
-  has_many :chatrooms
+  has_many :chatrooms, dependent: :destroy
   has_many :playings
-  has_many :games , through: :playings
+  has_many :games , through: :playings, dependent: :destroy
 
   has_many :gamers_groups
-  has_many :members, through: :gamers_groups, source: :gamer
+  has_many :members, through: :gamers_groups, source: :gamer, dependent: :destroy
 
 
   belongs_to :creator,class_name: "Gamer"
